a47722f05d3b4427fd8d88f12bbcfe9bae0544d6
Muhammet Çeneli <muhammetceneli@gmail.com>
2020-02-08 12:06:07 +0300

    Update asalv3.c

--- asalv3.c
+++ asalv3.c
@@ -2,8 +2,10 @@
 #include<math.h>
 
 int main(){
-  int i,j,n,value,number,changed;
-  printf("hangi sayiya kadar? : ");
+  int i,j,n,value,number,changed,yaz;
+  printf("sayilar da yazilsin mi? (1/0)\n");
+  scanf("%d",&yaz);
+  printf("hangi sayiya kadar?\n");
   scanf("%d",&n);
   number=n;
   
@@ -19,10 +21,12 @@ int main(){
   array[2/8] &= ~(1 << (2%8));
   ++number;
   int nn=sqrt(n);
+  if(yaz==1){printf("2");nn=n;}  
   
   for(i=3;i<nn+1;i++){
     value = (array[i/8] & (1 << (i%8))) != 0;
     if(value==0){
+      if(yaz==1){printf(",%d",i);}
       for(j=i*i;j<=n;j=j+2*i){
         changed = (array[j/8] & (1 << (j%8))) != 0;
         if(changed == 0){
